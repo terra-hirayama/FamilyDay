3dc0db8624da60cd809175e441752d6852cf6f86
terra-hirayama <41419373+terra-hirayama@users.noreply.github.com>
2018-07-25 16:50:57 +0900

    Update robot_button.py

--- robot_button.py
+++ robot_button.py
@@ -1,11 +1,12 @@
 from rrb3 import *
 import robot_controller as robot
+import time
 
 def main():
     rr = RRB3()
 
     while True:
-        print(rr.sw1_closed())
+        time.sleep(0.1)
         if rr.sw1_closed():
             robot.main()
 
