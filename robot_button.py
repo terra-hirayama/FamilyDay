4a16741ce248f71b5399001810033d613fc0cfe8
terra-hirayama <41419373+terra-hirayama@users.noreply.github.com>
2018-07-25 14:49:42 +0900

    Update robot_button.py

--- robot_button.py
+++ robot_button.py
@@ -1,9 +1,16 @@
 from rrb3 import *
 import robot_controller as robot
 
+def main():
+    rr = RRB3()
 
-rr = RRB3()
+    while True:
+        if rr.sw1_closed():
+            print('button')
+            robot.main()
 
-while True:
-    if rr.sw1_closed():
-        robot.main()
+        if rr.sw1_closed():
+            rr.stop()
+
+if __name__ == '__main__':
+    main()
