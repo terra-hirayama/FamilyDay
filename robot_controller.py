009b5addaefb80fc41f56a8252709f9a236e29e3
terra-hirayama <41419373+terra-hirayama@users.noreply.github.com>
2018-07-30 17:21:10 +0900

    Update robot_controller.py

--- robot_controller.py
+++ robot_controller.py
@@ -23,8 +23,9 @@ rr = rrb.RRB3(BATTERY_VOLTS, MOTOR_VOLTS)
 half_speed = 0.2
 back_speed = 0.1
 
+start_flag = False
 def command(ws, message):
-   
+    global start_flag
     if ',' in message:
         message = message.split(',')[1].replace('"', '').replace(']', '')
         
